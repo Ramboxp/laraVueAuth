545bfa45b4478bef1346f40c10f155d6df3c3909
Oscar Parra Rodriguez <oprodriguez1990@gmail.com>
2023-07-28 13:21:10 +0200

    Add new styles

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,34 +1,35 @@
 <template>
-    <div class="container h-100">
-        <div class="row h-100 align-items-center">
-            <div class="col-12 col-md-6 offset-md-3">
-                <div class="card shadow sm">
+    <div class="container">
+        <div class="row justify-content-center align-items-center min-vh-100">
+            <div class="col-12 col-md-6">
+                <div class="card shadow">
                     <div class="card-body">
-                        <h1 class="text-center">Login</h1>
-                        <hr/>
-                        <form action="javascript:void(0)" class="row" method="post">
-                            <div class="col-12" v-if="Object.keys(validationErrors).length > 0">
-                                <div class="alert alert-danger">
-                                    <ul class="mb-0">
-                                        <li v-for="(value, key) in validationErrors" :key="key">{{ value[0] }}</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="form-group col-12">
+                        <h2 class="text-center">Login</h2>
+                        <div class="alert alert-danger" v-if="Object.keys(validationErrors).length > 0">
+                            <ul class="mb-0">
+                                <li v-for="(value, key) in validationErrors" :key="key">{{ value[0] }}</li>
+                            </ul>
+                        </div>
+                        <form action="javascript:void(0)" method="post">
+                            <div class="form-group">
                                 <label for="email" class="font-weight-bold">Email</label>
-                                <input type="text" v-model="auth.email" name="email" id="email" class="form-control">
+                                <input type="email" v-model="auth.email" name="aa" id="email" class="form-control">
                             </div>
-                            <div class="form-group col-12 my-2">
+                            <div class="form-group">
                                 <label for="password" class="font-weight-bold">Password</label>
-                                <input type="password" v-model="auth.password" name="password" id="password" class="form-control">
+                                <input type="password" v-model="auth.password" name="password" id="password"
+                                    class="form-control">
                             </div>
-                            <div class="col-12 mb-2">
-                                <button type="submit" :disabled="processing" @click="login" class="btn btn-primary btn-block">
+                            <br>
+                            <div class="mb-2">
+                                <button type="submit" :disabled="processing" @click="login"
+                                    class="btn btn-primary btn-block">
                                     {{ processing ? "Please wait" : "Login" }}
                                 </button>
                             </div>
-                            <div class="col-12 text-center">
-                                <label>Don't have an account? <router-link :to="{name:'register'}">Register Now!</router-link></label>
+                            <div class="text-center">
+                                <label>Not registered &#128558;? <router-link :to="{ name: 'register' }"> Register
+                                        now!?</router-link></label>
                             </div>
                         </form>
                     </div>
@@ -37,41 +38,48 @@
         </div>
     </div>
 </template>
+  
 
 <script>
 import { mapActions } from 'vuex'
 export default {
-    name:"login",
-    data(){
+    name: "login",
+    data() {
         return {
-            auth:{
-                email:"",
-                password:""
+            auth: {
+                email: "",
+                password: ""
             },
-            validationErrors:{},
-            processing:false
+            validationErrors: {},
+            processing: false
         }
     },
-    methods:{
+    methods: {
         ...mapActions({
-            signIn:'auth/login'
+            signIn: 'auth/login'
         }),
-        async login(){
+        async login() {
             this.processing = true
             await axios.get('/sanctum/csrf-cookie')
-            await axios.post('/login',this.auth).then(({data})=>{
+            await axios.post('/login', this.auth).then(({ data }) => {
                 this.signIn()
-            }).catch(({response})=>{
-                if(response.status===422){
+            }).catch(({ response }) => {
+                if (response.status === 422) {
                     this.validationErrors = response.data.errors
-                }else{
+                } else {
                     this.validationErrors = {}
                     alert(response.data.message)
                 }
-            }).finally(()=>{
+            }).finally(() => {
                 this.processing = false
             })
         },
     }
 }
 </script>
+
+<style>
+.min-vh-100 {
+    min-height: 100vh;
+}
+</style> 
