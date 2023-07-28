07390078b42b4aea70fce28b10707c2a21bfba9a
Oscar Parra Rodriguez <oprodriguez1990@gmail.com>
2023-07-28 15:41:13 +0200

    Finito

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -1,42 +1,45 @@
 <template>
-    <div class="container h-100">
-        <div class="row h-100 align-items-center">
-            <div class="col-12 col-md-6 offset-md-3">
-                <div class="card shadow sm">
+    <div class="container">
+        <div class="row justify-content-center align-items-center vh-100">
+            <div class="col-12 col-md-8 col-lg-6">
+                <div class="card shadow">
                     <div class="card-body">
                         <h1 class="text-center">Register</h1>
-                        <hr/>
-                        <form action="javascript:void(0)" @submit="register" class="row" method="post">
-                            <div class="col-12" v-if="Object.keys(validationErrors).length > 0">
-                                <div class="alert alert-danger">
-                                    <ul class="mb-0">
-                                        <li v-for="(value, key) in validationErrors" :key="key">{{ value[0] }}</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="form-group col-12">
+                        <hr />
+                        <div class="alert alert-danger" v-if="Object.keys(validationErrors).length > 0">
+                            <ul class="mb-0">
+                                <li v-for="(value, key) in validationErrors" :key="key">{{ value[0] }}</li>
+                            </ul>
+                        </div>
+                        <form :key="formKey" @submit.prevent="register" method="post">
+                            <div class="form-group">
                                 <label for="name" class="font-weight-bold">Name</label>
-                                <input type="text" name="name" v-model="user.name" id="name" placeholder="Enter name" class="form-control">
+                                <input type="text" name="name" v-model="user.name" id="name" placeholder="Enter name"
+                                    class="form-control" />
                             </div>
-                            <div class="form-group col-12 my-2">
+                            <div class="form-group">
                                 <label for="email" class="font-weight-bold">Email</label>
-                                <input type="text" name="email" v-model="user.email" id="email" placeholder="Enter Email" class="form-control">
+                                <input type="text" name="email" v-model="user.email" id="email" placeholder="Enter Email"
+                                    class="form-control" />
                             </div>
-                            <div class="form-group col-12">
+                            <div class="form-group">
                                 <label for="password" class="font-weight-bold">Password</label>
-                                <input type="password" name="password" v-model="user.password" id="password" placeholder="Enter Password" class="form-control">
+                                <input type="password" name="password" v-model="user.password" id="password"
+                                    placeholder="Enter Password" class="form-control" />
                             </div>
-                            <div class="form-group col-12 my-2">
+                            <div class="form-group">
                                 <label for="password_confirmation" class="font-weight-bold">Confirm Password</label>
-                                <input type="password_confirmation" name="password_confirmation" v-model="user.password_confirmation" id="password_confirmation" placeholder="Enter Password" class="form-control">
+                                <input type="password" name="password_confirmation" v-model="user.password_confirmation"
+                                    id="password_confirmation" placeholder="Enter Password" class="form-control" />
                             </div>
-                            <div class="col-12 mb-2">
+                            <div class="form-group text-center mt-4">
                                 <button type="submit" :disabled="processing" class="btn btn-primary btn-block">
                                     {{ processing ? "Please wait" : "Register" }}
                                 </button>
                             </div>
-                            <div class="col-12 text-center">
-                                <label>Already have an account? <router-link :to="{name:'login'}">Login Now!</router-link></label>
+                            <div class="form-group text-center mt-2">
+                                <label>Already have an account? <router-link :to="{ name: 'login' }">Login
+                                        Now!</router-link></label>
                             </div>
                         </form>
                     </div>
@@ -49,37 +52,38 @@
 <script>
 import { mapActions } from 'vuex'
 export default {
-    name:'register',
-    data(){
+    name: 'register',
+    data() {
         return {
-            user:{
-                name:"",
-                email:"",
-                password:"",
-                password_confirmation:""
+            formKey: 0, // Inicializa la clave del formulario
+            user: {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
             },
-            validationErrors:{},
-            processing:false
-        }
+            processing: false,
+            validationErrors: {},
+        };
     },
-    methods:{
+    methods: {
         ...mapActions({
-            signIn:'auth/login'
+            signIn: 'auth/login'
         }),
-        async register(){
+        async register() {
             this.processing = true
             await axios.get('/sanctum/csrf-cookie')
-            await axios.post('/register',this.user).then(response=>{
+            await axios.post('/register', this.user).then(response => {
                 this.validationErrors = {}
                 this.signIn()
-            }).catch(({response})=>{
-                if(response.status===422){
+            }).catch(({ response }) => {
+                if (response.status === 422) {
                     this.validationErrors = response.data.errors
-                }else{
+                } else {
                     this.validationErrors = {}
                     alert(response.data.message)
                 }
-            }).finally(()=>{
+            }).finally(() => {
                 this.processing = false
             })
         }
